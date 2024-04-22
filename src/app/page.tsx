abcfee66fc11f9fd53933e375f6cb1cc5234ac19
4321Music <4321musicproduction@gmail.com>
2024-04-22 16:54:47 +0800

    icon added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
+
+const perks = [
+  {
+    name: 'Instant Devlivery',
+    Icon: ArrowDownToLine,
+    description: "Get your assests delivery yo your email in seconds"
+  },
+  {
+    name: 'Guaranteed Quality',
+    Icon: CheckCircle,
+    description: "Every assets ou our platform is highest quality standards"
+  },
+  {
+    name: 'For the Planet',
+    Icon: Leaf,
+    description: "We've pledged 1% of sales"
+  },
+]
 
 export default function Home() {
     return (
+      <>
         <MaxWidthWrapper>
           <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
             <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
@@ -20,8 +40,35 @@ export default function Home() {
               <Link href="/products" className={ buttonVariants() }>
                 Borwse Trending
               </Link>
+              <Button variant='ghost'>Our quality promise &rarr;</Button>
             </div>
         </div>
+
+        {/* { TODO: List products} */}
         </MaxWidthWrapper>
+
+        <section className="border-t border-gray-20 bg-gray-50">
+          <MaxWidthWrapper className="py-20">
+          <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'>
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className='text-center md:flex md:items-start md:text-left lg:block lg:text-center'>
+                <div className='md:flex-shrink-0 flex justify-center'>
+                  <div className='h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900'>
+                    {<perk.Icon className='w-1/3 h-1/3' />}
+                  </div>
+                </div>
+
+
+                </div>
+              ))}
+
+
+            </div>
+
+          </MaxWidthWrapper>
+          </section>
+      </>
     )
 }
